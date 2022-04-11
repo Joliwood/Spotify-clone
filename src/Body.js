aef87c50e16ffe726a86e4659a4903b731aa0492
Joliwood <jolibois.contact@gmail.com>
2022-04-11 18:18:13 +0200

    Spotify clone V1 FINAL

--- src/Body.js
+++ src/Body.js
@@ -2,6 +2,11 @@ import React from 'react';
 import "./Body.css";
 import Header from "./Header";
 import { useDataLayerValue } from "./DataLayer";
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import SongRow from "./SongRow";
+
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
@@ -19,8 +24,16 @@ function Body({ spotify }) {
         </div>
       </div>
 
-      <div className="body_songs">
-        pleins de trucs
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle"/>
+          <FavoriteIcon fontSize="Large"/>
+          <MoreHorizIcon />
+        </div>
+
+        {discover_weekly?.tracks.items.map(items => (
+          <SongRow track={items.track} />
+        ))}
       </div>
 
     </div>
